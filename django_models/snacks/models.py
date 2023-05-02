915482df3f2b9bea0f452a66ea545b3c0c8e8f40
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2023-05-02 08:47:50 -0600

    site working, all tests passing

---
+++ django_models/snacks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    rating = models.IntegerField(default=0)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
